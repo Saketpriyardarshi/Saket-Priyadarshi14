acbbb0e24dcacee4e6676dc98a6a30cb24d7414d
Saketpriyardarshi <104380198+Saketpriyardarshi@users.noreply.github.com>
2022-05-12 22:20:15 +0530

    Add files via upload

---
+++ Answer assignment 3.py
@@ -0,0 +1,51 @@
+Q1
+Enter a number6
+Binary equivalent of 6 is 110
+Q2
+Enter expression:43/8
+43/8 = 5.375
+Q3
+Enter an integer2
+a) 20
+b) 1.0
+enter radius76
+c) 72583.35666853857
+Enter angle 1:30
+Enter angle 1:45
+d) 7.538906031854524
+Enter x coordinate of first point:6
+Enter y coordinate of first point:2
+Enter x coordinate of second point:5
+Enter y coordinate of second point:4
+e) -2.0
+Q4
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Q5
+Enter number of carbon atoms in molecule:4
+Enter number of hydrogen atoms in molecule:2
+Enter number of oxygen atoms in molecule:7
+The molecular weight of the compound is 162.05448
